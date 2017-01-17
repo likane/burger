3455eb8e0f592a376e8d6c3b4a30fa823e493c0e
Louis Kane <louiskane@1234.local>
2017-01-16 22:15:47 -0800

    first commit

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host:'localhost',
+	user:'root',
+	password:'55capone',
+	database:'burgers_db'
+});
+
+conection.connect(function(err){
+	if (err){
+		console.error("error connecting: " + err.stack);
+		console.log(":-{(");
+		return;
+	}
+
+	console.log('connected as ' + connection.threadId);
+
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('bacon burger');
+INSERT INTO burgers (burger_name) VALUES ('bison burger');
